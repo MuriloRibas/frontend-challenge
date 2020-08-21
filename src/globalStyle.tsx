8f12ec47d66da2f929809473737438dcddc6afb8
MuriloRibas <muriloribasss@hotmail.com>
2020-08-21 18:03:00 -0300

    add both gobalStyle and theme

---
+++ src/globalStyle.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+    * {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 16px;
+        padding: 0;
+        margin: 0;
+    }
+`;
---
+++ src/theme.tsx
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+        primary: '#212121'
+    },
+    font: {
+        size: {
+            default: '16px',
+            label: '14px'
+        },
+        weight: {
+            regular: 'normal',
+            bold: '600'
+        }
+    }
+}
