6329a400eb503cfe75fbda89f4fe60024f4374ee
MuriloRibas <muriloribasss@hotmail.com>
2020-08-24 11:22:51 -0300

    att naversList layout

--- src/components/naversList/styles.tsx
+++ src/components/naversList/styles.tsx
@@ -4,12 +4,18 @@ export const NaversListContainer = styled.div`
     width: 100%;
     height: 120vh;
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-start;
     flex-flow: row wrap;
+    & > * {
+        margin: 0px 32px 32px 0px;
+    }
     /* & > :not(:last-child) {
         margin-right: 32px;
     } */
     @media (max-width: 640px) {
         justify-content: center;
+        & > *{
+            margin: 0px 0px 32px 0px;
+        }
     }
 `;
